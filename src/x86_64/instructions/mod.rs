c8cba8182b9e0bebd1863eeba9d477dfbddec7f3
KarimAllah Ahmed <karim.allah.ahmed@gmail.com>
2019-10-12 14:52:49 +0200

    x86_64: vmx: Introduce vmxon/vmxoff instructions
    Signed-off-by: KarimAllah Ahmed <karim.allah.ahmed@gmail.com>

--- src/x86_64/instructions/mod.rs
+++ src/x86_64/instructions/mod.rs
@@ -1,1 +1,24 @@
+use crate::{AlignedAddress, SHIFT_4K};
+
 pub mod vmcs;
+
+pub struct VMX;
+
+impl VMX {
+    pub unsafe fn vmxon(address: u64) -> bool {
+        if !address.aligned(SHIFT_4K) {
+            return false;
+        }
+
+        asm!("vmxon $0":: "m" (address));
+        true
+    }
+
+    pub unsafe fn vmxoff() {
+        asm!("vmxoff");
+    }
+
+    pub unsafe fn vmcall() {}
+
+    pub unsafe fn vmfunc() {}
+}
