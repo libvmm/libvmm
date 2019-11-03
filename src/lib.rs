a39579a262f6ac982882a3b5b0c48391c6394a31
KarimAllah Ahmed <karim.allah.ahmed@gmail.com>
2019-11-03 21:17:24 +0100

    Add PAGE_1G and fix the definition of PAGE_4K
    Signed-off-by: KarimAllah Ahmed <karim.allah.ahmed@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -8,17 +8,18 @@ extern crate libvmm_macros;
 #[cfg(target_arch = "x86_64")]
 pub mod x86_64;
 
-const SHIFT_4K: u8 = 12;
-const SHIFT_2M: u8 = 21;
-const SHIFT_1G: u8 = 30;
-const SHIFT_512G: u8 = 39;
+pub const SHIFT_4K: u8 = 12;
+pub const SHIFT_2M: u8 = 21;
+pub const SHIFT_1G: u8 = 30;
+pub const SHIFT_512G: u8 = 39;
 
-const KiB: usize = 1024;
-const MiB: usize = 0x100000;
+pub const KiB: usize = 1024;
+pub const MiB: usize = 0x100000;
 const GiB: usize = 0x40000000;
 
-const PAGE_4K: usize = 2 * KiB;
-const PAGE_2M: usize = 2 * MiB;
+pub const PAGE_4K: usize = 4 * KiB;
+pub const PAGE_2M: usize = 2 * MiB;
+pub const PAGE_1G: usize = 1 * GiB;
 
 trait AlignedAddress {
     fn aligned(&self, shift: u8) -> bool;
